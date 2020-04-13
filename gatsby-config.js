8a2ba19ebeb5307c1fe648d060b3e16170661d6a
Joseph Martinsen <joseph@martinsen.com>
2020-04-12 19:11:51 -0500

    Add M favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#FFFFFF`,
         theme_color: `#4a4a4a`,
         display: `standalone`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/MarryinMartinsenFacIcon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sass`,
