fd0adfec4454c0f6a42b15c474910eae79223965
Joseph Martinsen <joseph@martinsen.com>
2020-04-14 22:04:38 -0500

    Add loading and setup form

---
+++ src/components/RSVP/FormEntry.js
@@ -0,0 +1,71 @@
+import React from "react"
+
+import "./FormEntry.scss"
+
+// const FormEntry = () => (
+//   <div className="FormEntry">
+//     <div className="name">
+//       <input type="text" />
+//     </div>
+//     <div className="attending">
+//       <input type="checkbox" />
+//     </div>
+//     <div className="diet">
+//       <input type="checkbox" />
+//       <input type="checkbox" />
+//       <input type="checkbox" />
+//     </div>
+//   </div>
+// )
+
+const FormEntry = () => (
+  <> </>
+  // <div className="FormEntry">
+  //   <div className="table-container" role="table" aria-label="Destinations">
+  //     <div className="flex-table header" role="rowgroup">
+  //       <div className="flex-row first" role="columnheader">
+  //         Name
+  //       </div>
+  //       <div className="flex-row" role="columnheader">
+  //         Attending
+  //       </div>
+  //       <div className="flex-row" role="columnheader">
+  //         Allergies
+  //       </div>
+  //     </div>
+  //     <div className="flex-table row" role="rowgroup">
+  //       <div className="flex-row first" role="cell">
+  //         <input type="text" />
+  //       </div>
+  //       <div className="flex-row" role="cell">
+  //         <fieldset>
+  //           <input type="radio" name="attending" id="foo_bar" />
+  //           <label for="foo_bar">Yes</label>
+
+  //           <input type="radio" name="attending" id="foo_baz" />
+  //           <label for="foo_baz">No</label>
+  //         </fieldset>
+  //       </div>
+  //       <div className="flex-row" role="cell">
+  //         <fieldset>
+  //           <input type="checkbox" name="diet" value="gf" id="foo_bar" />
+  //           <label for="foo_bar">GF</label>
+
+  //           <input type="checkbox" name="diet" value="dairy" id="foo_baz" />
+  //           <label for="foo_baz">Dairy</label>
+
+  //           <input
+  //             type="checkbox"
+  //             name="diet"
+  //             value="vegetarian"
+  //             id="foo_baz"
+  //           />
+  //           <label for="foo_baz">Vegetarian</label>
+  //         </fieldset>
+  //       </div>
+  //     </div>
+  //   </div>
+  // </div>
+)
+
+export default FormEntry
--- src/pages/rsvp.js
+++ src/pages/rsvp.js
@@ -1,8 +1,13 @@
 import React, { useState } from "react"
 import classNames from "classnames"
 import ky from "ky"
+import { usePromiseTracker } from "react-promise-tracker"
+import { trackPromise } from "react-promise-tracker"
+import BeatLoader from "react-spinners/BeatLoader"
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import FormEntry from "../components/RSVP/FormEntry"
 
 import "./rsvp.scss"
 
@@ -12,33 +17,47 @@ const noResultMsg = "No result found"
 const RSVPPage = () => {
   const [searchState, setSearchState] = useState({
     hasSearched: false,
-    foundMatch: [],
+    foundMatch: false,
     searchText: "",
   })
+  const { promiseInProgress } = usePromiseTracker()
 
   /**
    * Search for RSVP invitation
    */
   const handleSearch = async () => {
-    // const parsed = await ky.get("http://localhost:3000/api/register").json()
-    const parsed = await ky
-      .get("https://registry-two.now.sh/api/register")
-      .json()
-
-    console.log(parsed)
-    setSearchState(state => ({
-      ...state,
-      hasSearched: true,
-      foundMatch: parsed.rsvps.filter(
-        ({ firstName }) => firstName === state.searchText
-      ),
-    }))
+    trackPromise(
+      ky
+        .get("https://registry-two.now.sh/api/register", {
+          // .get("http://localhost:3000/api/canRegister", {
+          searchParams: new URLSearchParams({ name: searchState.searchText }),
+        })
+        .json()
+        .then(parsed => {
+          console.log(parsed)
+          setSearchState(state => ({
+            ...state,
+            hasSearched: true,
+            foundMatch: parsed.success,
+            match: parsed.rsvp,
+          }))
+        })
+    )
   }
 
-  const genMsg = () =>
-    searchState.foundMatch.length > 0
-      ? `Found RSVP for ${searchState.foundMatch[0].firstName} ${searchState.foundMatch[0].lastName}!`
-      : noResultMsg
+  const genMsg = () => {
+    if (!searchState.hasSearched) {
+      return null
+    }
+    return searchState.foundMatch ? (
+      <>
+        <p>{`Found RSVP for ${searchState.match.firstName} ${searchState.match.lastName}!`}</p>
+        <p>{`Please come back later to RSVP when Joseph has the service up and running :)`}</p>
+      </>
+    ) : (
+      noResultMsg
+    )
+  }
 
   /**
    * Search with Enter key
@@ -55,8 +74,8 @@ const RSVPPage = () => {
     setSearchState(state => ({ ...state, searchText }))
   }
 
-  const correct = searchState.hasSearched && searchState.foundMatch.length > 0
-  const wrong = searchState.hasSearched && !searchState.foundMatch.length > 0
+  const correct = searchState.hasSearched && searchState.foundMatch
+  const wrong = searchState.hasSearched && !searchState.foundMatch
 
   return (
     <Layout location={pageTitle}>
@@ -88,8 +107,14 @@ const RSVPPage = () => {
         <button onClick={handleSearch}>Search</button>
       </div>
 
+      {/* Search Result Message */}
       <div className={classNames({ message: true, correct, wrong })}>
-        <p>{genMsg()}</p>
+        {promiseInProgress === true ? <BeatLoader /> : genMsg()}
+      </div>
+
+      {/* RSVP Form */}
+      <div className="rsvpForm">
+        <FormEntry />
       </div>
     </Layout>
   )
