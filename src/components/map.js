05734893bf9e0daaa3b2199ccb36062a2983b157
Joseph Martinsen <joseph@martinsen.com>
2020-05-28 22:39:46 -0500

    More progress on Wedding Map

--- src/components/map.js
+++ src/components/map.js
@@ -1,21 +1,39 @@
 import React from "react"
-import { GoogleMap, Marker, LoadScript } from "@react-google-maps/api"
+import {
+  GoogleMap,
+  Marker,
+  LoadScript,
+  InfoWindow,
+} from "@react-google-maps/api"
 
 const containerStyle = {
   width: "100%",
-  height: "30em",
+  height: "100%",
 }
 
 const center = {
-  lat: 29.42585,
-  lng: -98.543517,
+  lat: 29.42552,
+  lng: -98.54315,
 }
 
 function Map() {
   return (
     <LoadScript googleMapsApiKey={process.env.GATSBY_MAP_KEY}>
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
-        <Marker position={center} />
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={12}>
+        <InfoWindow position={center}>
+          <div className="infoWindow">
+            <b>Sacred Heart Chapel</b>
+            <p>Pvt Rd</p>
+            <p>San Antonio, TX 78237</p>
+            <a
+              href="https://maps.google.com/maps?ll=29.426978,-98.54315&z=17&t=m&hl=en-US&gl=US&mapclient=apiv3&cid=4826326906391070726"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <p>View on Google Maps</p>
+            </a>
+          </div>
+        </InfoWindow>
       </GoogleMap>
     </LoadScript>
   )
--- src/pages/wedding.js
+++ src/pages/wedding.js
@@ -4,14 +4,11 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Map from "../components/map"
 
+import "./wedding.scss"
+
 const pageTitle = "Wedding"
 
-const containerStyle = {
-  display: "flex",
-  justifyContent: "center",
-  width: "100%",
-  height: "40em",
-}
+const containerStyle = {}
 
 const WeddingPage = () => {
   console.log(process.env.GATSBY_LOL_TEST)
@@ -19,7 +16,9 @@ const WeddingPage = () => {
     <Layout location={pageTitle}>
       <SEO title={pageTitle} />
       <h1 className="title cursive">Wedding</h1>
-      <div style={containerStyle}>{<Map />}</div>
+      <div className="mapContainer" style={containerStyle}>
+        {<Map />}
+      </div>
     </Layout>
   )
 }
