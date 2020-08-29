d7316962b3c57ab12c5854b11ff300591e7fe50a
Joseph Martinsen <joseph@martinsen.com>
2020-08-29 01:08:25 -0500

    add links to the streams

---
+++ src/images/f.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+const Image = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "f.png" }) {
+        childImageSharp {
+          fluid(maxHeight: 1000) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <Img
+      fluid={{
+        ...data.placeholderImage.childImageSharp.fluid,
+      }}
+    />
+  )
+}
+
+export default Image
--- src/pages/watch.js
+++ src/pages/watch.js
@@ -3,6 +3,9 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import F from "../images/f"
+import YT from "../images/yt"
+
 import "./watch.scss"
 
 const pageTitle = "Watch"
@@ -12,22 +15,34 @@ const WatchPage = () => (
     <SEO title={pageTitle} />
     <h1 className="title cursive">Watch</h1>
 
-    {process.env.GATSBY_STREAM_URL ? (
-      <div className="videoWrapper">
-        <iframe
-          width="560"
-          height="315"
-          modestbranding="1"
-          title="wedding-stream"
-          src={`${process.env.GATSBY_STREAM_URL}?modestbranding=1&color=white`}
-          frameBorder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
+    <p>
+      Thank you to all of our friends and family joining us virtually, while we
+      wish we could be with you in person, we are happy you are able to
+      celebrate this special day with us! Thanks for all of your love and
+      support today and throughout our lives!
+    </p>
+    <h2>Click to watch on one of the two platforms below!</h2>
+
+    <div className="row">
+      <div className="item">
+        <a
+          href="https://www.facebook.com/czdigitalmedia/posts/3232718040098962"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <F />
+        </a>
       </div>
-    ) : (
-      <h3>Come back soon to watch the stream!</h3>
-    )}
+      <div className="item">
+        <a
+          href="https://www.youtube.com/watch?v=Kl-26caWN4s"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <YT />
+        </a>
+      </div>
+    </div>
   </Layout>
 )
 
