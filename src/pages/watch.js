c582bf7c9775396c5103cce37787d1f3c622831a
Joseph Martinsen <joseph@martinsen.com>
2021-04-29 10:20:39 -0500

    components

--- src/pages/watch.js
+++ src/pages/watch.js
@@ -2,6 +2,7 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Video from "../components/video"
 
 import "./watch.scss"
 
@@ -10,35 +11,12 @@ const pageTitle = "Watch"
 const WatchPage = () => (
   <Layout location={pageTitle}>
     <SEO title={pageTitle} />
-    <h1 className="cursive">Livestream</h1>
 
-    <div className="videoWrapper">
-      <iframe
-        title="livestream"
-        width="560"
-        height="315"
-        src="https://www.youtube-nocookie.com/embed/Kl-26caWN4s"
-        frameBorder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      />
-    </div>
+    <h1 className="cursive">Livestream</h1>
+    <Video src="https://www.youtube-nocookie.com/embed/Kl-26caWN4s" />
 
     <h1 className="cursive">Highlights</h1>
-
-    <div className="videoWrapper">
-      <div>
-        <iframe
-          title="highlights"
-          width="560"
-          height="315"
-          src="https://www.youtube-nocookie.com/embed/jVFTowOKGuE"
-          frameBorder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        />
-      </div>
-    </div>
+    <Video src="https://www.youtube-nocookie.com/embed/jVFTowOKGuE" />
   </Layout>
 )
 
